f9b77d46444b071d0ce9611b294fb3c99a8e0887
Arzaq <arzaqzafer123@gmali.com>
2023-11-25 23:23:06 +0300

    Create Migration

---
+++ database/migrations/2023_11_25_191914_create_artical_tags_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('artical_tags', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('tags_id')->unsigned();
+            $table->bigInteger('artical_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('artical_tags');
+    }
+};
